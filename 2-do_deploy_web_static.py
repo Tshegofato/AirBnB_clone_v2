b1a6d782fff6b8da04139881ec0d49303c89079a
Tshegofato <basetsana_12@icloud.com>
2024-02-06 10:22:06 -0800

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script to distribute an archive to web servers using do_deploy function
 """
 
-from fabric.api import put, run, env
+from fabric.api import run, put, env
 from os.path import exists
+
 env.hosts = ['18.208.119.244', '100.24.206.26']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """
+    Distribute an archive to web servers.
+    Args:
+        archive_path (str): Path to the archive to be deployed.
+    Returns:
+        bool: True if all operations have been done correctly, False otherwise.
+    """
+    if not exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        archive_name = archive_path.split('/')[-1]
+        archive_base = archive_name.split('.')[0]
+        remote_path = "/tmp/{}".format(archive_name)
+
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(archive_base))
+        run("sudo tar -xzf {} -C /data/web_static/releases/{}/"
+            .format(remote_path, archive_base))
+        run("sudo rm {}".format(remote_path))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(archive_base, archive_base))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(archive_base))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archive_base))
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
